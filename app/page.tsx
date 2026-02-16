dd5c0d46dd4e2c830cb00b8a22759ccc5222cae0
Zellie Macabata <1995zellie@gmail.com>
2026-02-16 14:30:28 -0500

    get rid of template grey background

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Stopwatch from "@/components/stopwatch";
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen items-center justify-center font-sans">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black ">
         <Stopwatch />
       </main>
